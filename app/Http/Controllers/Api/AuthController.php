2e902ba2387a5ca9359410c6618ffba651c17396
mar-babu <ar_cse@yahoo.com>
2025-05-05 16:52:53 +0600

    api setup and auth api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:191',
+                'email' => 'required|string|email|max:191|unique:users',
+                'password' => 'required|string|confirmed|min:8',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['status' => false, 'message' => 'Validation error', 'errors' => $validator->errors()], 422);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json(['status' => true, 'message' => 'User registered successfully.', 'token' => $token], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required|string',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['status' => false, 'message' => 'Validation error', 'errors' => $validator->errors()], 422);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, Hash::make($request->password))) {
+                return response()->json(['status' => false, 'message' => 'Invalid credentials.'], 401);
+            }
+
+            $user->tokens()->delete();
+            $token = $user->createToken($request->userAgent())->plainTextToken;
+
+            $isVerified = ($user->email_verified == 1) ? true : false;
+
+            return response()->json(['status' => true, 'message' => 'User logged in successfully', 'token' => $token, 'isVerified' => $isVerified], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+
+    }
+}
